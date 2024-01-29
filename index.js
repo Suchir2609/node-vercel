94aee8f733b6079d138c77d2ccb33ba712e9489f
suchir Gupta <suchirgupta26@gmail.com>
2024-01-29 15:09:07 +0530

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send('this was deployed on vercel')
+    res.send('this was deployed on vercel successfully by Suchir Gupta from Bits Pilani')
 })
 
 app.listen(3000, ()=>{
